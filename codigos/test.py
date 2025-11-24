2082a352efa116be079c370d7ff1ac614ce2d592
AdrianRodriguez04 <adrianrodriguez34@aragon.unam.mx>
2025-11-24 17:26:37 -0600

    REFACTOR: Modificación del archivo test.py para probar el nuevo programa

--- codigos/test.py
+++ codigos/test.py
@@ -1,5 +1,11 @@
 from _create_agent import create_agent
+from _use_agent import use_agent
 
-create_agent()
+#Primera ejecución para crear un nuevo agente
+#create_agent()
 
-create_agent()
+#Segunda ejecución para eliminar un agente si esta activo y despues crear un nuevo agente
+#create_agent()
+
+#Primera ejecución para intentar reusar un agente
+use_agent()
